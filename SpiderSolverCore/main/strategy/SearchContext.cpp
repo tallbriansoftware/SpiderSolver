d2a7c32aecf0062f51f4256a0125dfbec4be34f1
BrianChapman <brian.chapman62@gmail.com>
2025-12-20 17:57:57 -0800

    downgraded debug messages from search context

--- SpiderSolverCore/main/strategy/SearchContext.cpp
+++ SpiderSolverCore/main/strategy/SearchContext.cpp
@@ -35,37 +35,16 @@ std::vector<MoveCombo> SearchContext::GetMoves(const SpiderTableau& tableau)
 void SearchContext::AddParentPosition(const std::string& tabString)
 {
     auto count = m_parentList->PushTableau(tabString);
- //   std::cout << "Saving Parent: " << count << " " << tabString << "\n";
 }
 
 
 void SearchContext::RemoveParentPosition(const std::string& tabString)
 {
     auto listHeadString = m_parentList->PopTableau();
-    assert(tabString == listHeadString);
-//    std::cout << "Removing Parent: " << tabString << "\n";
-
-}
-
-namespace
-{
-    uint16_t Hash(const std::string& s)
-    {
-        uint16_t hash = 0;
-        for (char c : s)
-        {
-            uint16_t highBits = (hash & 0xC000);
-            hash += c;
-            hash <<= 2;
-            hash |= highBits >> 14;
-        }
-        return hash;
-    }
 }
 
 bool SearchContext::IsAParentPosition(const std::string& tabString) const
 {
-//    std::cout << "Checking Parent: " << tabString << "\n";
     return (m_parentList->FindRepeatIndex(tabString) > 0);
 }
 
@@ -76,9 +55,6 @@ bool SearchContext::TryFindSpiderNode(
     auto itr = m_nodeMap.find(tableauString);
     if (itr != m_nodeMap.end())
     {
-        int hash = Hash(tableauString);
- //       std::cout << " Hitting:  " << hash << " " << tableauString << "\n";
-
         node = itr->second;
         return true;
     }
@@ -87,9 +63,6 @@ bool SearchContext::TryFindSpiderNode(
 
 void SearchContext::AddSpiderNode(const SpiderNode& node)
 {
-    int hash = Hash(node.GetStringRep());
- //   std::cout << " Adding:  " << hash << " " << node.GetStringRep() << "\n";
-
 #ifdef _DEBUG
     SpiderNode dummy;
     bool val = TryFindSpiderNode(node.GetStringRep(), dummy);
@@ -100,9 +73,6 @@ void SearchContext::AddSpiderNode(const SpiderNode& node)
 
 bool SearchContext::RemoveSpiderNode(const std::string& tableauString)
 {
-    int hash = Hash(tableauString);
- //   std::cout << "Removing: " << hash << " " << tableauString << "\n";
-
     return 0 != m_nodeMap.erase(tableauString);
 }
 
