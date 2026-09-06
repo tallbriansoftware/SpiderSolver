890ea16aa958562b6fc0808ff286175fd616a811
BrianChapman <brian.chapman62@gmail.com>
2026-09-06 09:38:04 -0700

    updated data analysis tools and files

--- data/scp.cmd
+++ data/scp.cmd
@@ -1,3 +1,3 @@
-..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 1 --treeDepths 2-6 > r100-299_s1_t2-6.csv
-..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 2 --treeDepths 2-6 > r100-299_s2_t2-6.csv
-..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 4 --treeDepths 2-6 > r100-299_s4_t2-6.csv
+..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 1 --treeDepths 2-6 > Fixes.csv
+..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 2 --treeDepths 2-6 >> Fixes.csv
+..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 4 --treeDepths 2-6 >> Fixes.csv
