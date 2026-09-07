77e939339cd22419401ec3db2378b83bca27fb17
BrianChapman <brian.chapman62@gmail.com>
2026-09-07 11:34:44 -0700

    new data taking and analysis

--- data/scp.cmd
+++ data/scp.cmd
@@ -1,3 +1,4 @@
-..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 1 --treeDepths 2-6 > Fixes.csv
-..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 2 --treeDepths 2-6 >> Fixes.csv
-..\x64\Release\SpiderSolverMain.exe --limitSeconds 300 --randomSeeds 100-299 --suits 4 --treeDepths 2-6 >> Fixes.csv
+@echo OFF
+..\x64\Release\SpiderSolverMain.exe --limitSeconds 100 --randomSeeds 100-299 --suits 1 --treeDepths 2-6 
+..\x64\Release\SpiderSolverMain.exe --limitSeconds 100 --randomSeeds 100-299 --suits 2 --treeDepths 2-6 | findstr /v suits
+..\x64\Release\SpiderSolverMain.exe --limitSeconds 100 --randomSeeds 100-299 --suits 4 --treeDepths 2-6 | findstr /v suits
